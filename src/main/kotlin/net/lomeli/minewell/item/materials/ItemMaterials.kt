4e15f38c118d3f9b6c509c227e788e5bd82e0832
Lomeli12 <anthonylomeli11@gmail.com>
2018-10-24 00:23:41 -0700

    Missed a class for the last commit.

--- src/main/kotlin/net/lomeli/minewell/item/materials/ItemMaterials.kt
+++ src/main/kotlin/net/lomeli/minewell/item/materials/ItemMaterials.kt
@@ -1,4 +1,16 @@
 package net.lomeli.minewell.item.materials
 
-class ItemMaterials {
+import net.lomeli.minewell.item.ItemBase
+import net.minecraft.item.EnumRarity
+import net.minecraft.item.ItemStack
+
+class ItemMaterials(name: String): ItemBase(name) {
+    private var rarity = EnumRarity.COMMON
+
+    fun setRarity(rarity: EnumRarity): ItemMaterials {
+        this.rarity = rarity
+        return this
+    }
+
+    override fun getRarity(stack: ItemStack): EnumRarity = rarity
 }
